be1fd641ea573fef2b5b76e911af1c4434496608
sumaikakaira <122632122+sumaikakaira@users.noreply.github.com>
2023-04-14 23:55:53 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+//vowel_checker
+var city=prompt('what is your city?');
+if(city==="karachi"){
+   document.write('welcome to city of lights')
+};
+//question no 2
+var gender=prompt('what is your gender');
+if(gender==="male"){
+   document.write("good morning sir!")
+}
+else if(gender==="female"){
+document.write("good morning maam")
+}
+else if(gender==="not male or female"){
+   document.write("you are robot")
+};
+//ques 3
+var color=prompt('color of road traffic signal');
+   if(color==="red"){
+      document.write("must stop")
+   }
+   else if(color==='yellow'){
+      document.write("ready to move")
+   }
+   else if(color==='green'){
+      document.write('move now')
+   };
+   //ques 4
+   var current=prompt('fuel in car');
+if(current>"0.25"){
+   alert("please refill the fuel in your car")
+}
+else{
+  alert('not refill')
+}
+//ques 5
+var number=prompt('1to10');
+if(number==='10'){
+   document.write('bingo correct')
+}
+else if(number==="5")[
+   document.write("close number")
+]
+
+
+
+
